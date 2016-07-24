fdcae8154cb5cba82dd4322ac2282d67cb5a52a4
ch <changhanlee@gmail.com>
2016-07-23 20:26:42 -0400

    f

--- src/Main.hs
+++ src/Main.hs
@@ -7,5 +7,3 @@ root = Root "board" 0
 main = do
   putStrLn $ b root
   putStrLn $ parse_fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-  putStrLn $ pad $ parse_fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
-  putStrLn $ pretty $ pad $ parse_fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
--- src/Util.hs
+++ src/Util.hs
@@ -4,14 +4,16 @@ import Data.List.Split
 import Data.Char 
 import Data.List
 
-parse_fen x = intercalate "\n" $ map make_line $ splitOn "/" x
+parse_fen x = pad $ intercalate "\n" $ map make_line $ splitOn "/" x
 make_line x = intercalate "" $ map test x
 test x 
   | isDigit x = intercalate "" $ drop 1 $ splitOn "" $ replicate (digitToInt x) '.'
   | isAlpha x = [x]
 
 pad x = replicate 19 ' ' ++ "\n" ++
-  intercalate "" (map (\x->"  "++x++" \n") (splitOn "\n" x)) ++
+  intercalate "" (map (\x->" "++x++"\n") (splitOn "\n" x)) ++
   replicate 9 ' ' ++ "\n" ++ replicate 10 ' '
+
+unpad x = intercalate "\n" $ map (\x->(take 8 (drop 1 x))) (take 8 (drop 1 (splitOn "\n" x)))
 
 pretty x = intercalate " " $ splitOn "" x
