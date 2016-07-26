ba33132ace6620131a822392b0d62afb4803cbaf
ch <changhanlee@gmail.com>
2016-07-26 15:09:33 -0400

    now i know how to extract the move sorta

--- src/Search.hs
+++ src/Search.hs
@@ -5,21 +5,21 @@ import Node
 --{-
 
 ab d g n 
+  | d==0 = score n
   | d>0 = let 
     ge = gen_all n
-    ta = takeWhile (\x->x<g) $ map (negate . (ab (d-1) (1-g))) $ map (update n) ge
+    ta = takeWhile (\x->x<g) $ map (\x->negate (ab (d-1) (1-g) x)) $ map (update n) ge
     in if length ta < length ge then g else maximum ta 
-  | d==0 = score n
 
 frame u l d n
-  | u<=l = g
+  | u<=l+2 = g
   | u>l = let 
     cg = ab d g n
     in if cg==g then frame u g d n else frame g l d n 
   where g = div (u+l) 2
 
 frameio u l d n
-  | u<=l+1 = print (u,l,g)
+  | u<=l+2 = print (u,l,g)
   | u>l = do
       print (u,l,g)
       let cg = ab d g n
