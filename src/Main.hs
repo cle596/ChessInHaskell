a1338fbf984995a5a8cbb3db2e6e83fd779eac67
ch <changhanlee@gmail.com>
2016-07-27 09:45:33 -0400

    er

--- src/Main.hs
+++ src/Main.hs
@@ -10,13 +10,13 @@ root = Root True (parse_fen "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") (0,0)
 loop n = do
   putStrLn $ pretty $ board n
   let g = gen_all n
-  print $ map ttrans $ g
-  putStr "score: "
-  print $ score n
-  print $ ab 4 0 n 
+  --print $ map ttrans $ g
+  --putStr "score: "
+  --print $ score n
+  --print $ ab 4 0 n 
   --let bm = frame end (negate end) 4 n "null"
   --print bm
-  --frameio end (negate end) 3 n "null"
+  frameio end (negate end) 4 n "null"
   i <- getLine
   if elem (rrtrans i) g then loop (update n (rrtrans i)) else putStrLn "no"
 
